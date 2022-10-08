178ac4a632127bc09a1f5fa66b27bf5577080cb8
Manuel Erazo <elustondo129@gmail.com>
2022-10-08 16:44:59 -0500

    arduino succesfull

--- base/views/llavero_views.py
+++ base/views/llavero_views.py
@@ -1,16 +1,13 @@
 import serial, time
 
+
 def arduino():
-    port = serial.Serial('COM3', 9600, timeout=1)
+    port = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     time.sleep(2)
-    rawString = arduino.readline()
-    print(rawString)
-    
     c = 0
     hexa = ""
     while True:
         read = port.readline()
-        # print(read.strip())
         c += 1
         if c == 4:
             valor = read.strip()
@@ -18,8 +15,31 @@ def arduino():
             print(hexa)
         if c == 5:
             break
+    port.close()
+    return hexa
+
+
+def lectura_arduino():
+    arduino = serial.Serial('COM3', 9600)
+    time.sleep(2)
+    rawString = arduino.readline()
+    print(rawString)
+    c = 0
+    hexa = ""
+    while True:
+        sarduino = arduino.readline()
+        print(sarduino.strip())
+        #c = input("igrese ::")
+        c += 1
+        if c == 4:
+            valor = sarduino.strip()
+            hexa = str(valor).split(":  ")
+            hexa = hexa[1].replace("'", "")
+        if c == 5:  # en 5 pasos de al lectura del aurduino para el cilo de la lectura
+            # arduino.close()
+            break
     arduino.close()
     return hexa
 
 
-arduino()
+# arduino()
